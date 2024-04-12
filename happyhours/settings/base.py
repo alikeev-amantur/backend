794ba5d79bd73503e52f1d692907f0030e678999
unknown <amanturalikeev@gmail.com>
2024-04-12 20:53:23 +0600

    add dockefile&docker-compose

--- happyhours/settings/base.py
+++ happyhours/settings/base.py
@@ -86,11 +86,11 @@ WSGI_APPLICATION = "happyhours.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DATABASE_NAME'),
-        'USER': os.getenv('DATABASE_USER'),
-        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
-        'HOST': os.getenv('DATABASE_HOST'),
-        'PORT': os.getenv('DATABASE_PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
