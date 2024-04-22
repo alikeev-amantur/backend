6b25a959aed6a2132a658b774f92f172271f8939
unknown <amanturalikeev@gmail.com>
2024-04-22 23:18:53 +0600

    Fixed beverage for pylint

--- apps/beverage/views.py
+++ apps/beverage/views.py
@@ -2,10 +2,11 @@ from rest_framework import viewsets
 from rest_framework.exceptions import NotFound
 from rest_framework.generics import get_object_or_404
 
-from .filters import BeverageFilter
+# from .filters import BeverageFilter
 from .models import Category, Beverage
 from .serializers import CategorySerializer, BeverageSerializer
-from django_filters import rest_framework as filters
+
+# from django_filters import rest_framework as filters
 
 from ..partner.models import Establishment
 
@@ -28,9 +29,13 @@ class MenuViewSet(viewsets.ReadOnlyModelViewSet):
     def get_queryset(self):
         establishment_id = self.kwargs.get("establishment_id")
         establishment = get_object_or_404(Establishment, id=establishment_id)
-        return Beverage.objects.filter(establishment=establishment).select_related('category', 'establishment')
+        return Beverage.objects.filter(establishment=establishment).select_related(
+            "category", "establishment"
+        )
 
     def list(self, request, *args, **kwargs):
         if not self.get_queryset().exists():
-            raise NotFound("No beverages found for this establishment or establishment does not exist.")
+            raise NotFound(
+                "No beverages found for this establishment or establishment does not exist."
+            )
         return super().list(request, *args, **kwargs)
