2c28e31b66ea816c72c6c8a810b6afe293b83824
unknown <amanturalikeev@gmail.com>
2024-04-29 12:48:11 +0600

    Update swagger docs

--- happyhours/settings/base.py
+++ happyhours/settings/base.py
@@ -145,6 +145,7 @@ SPECTACULAR_SETTINGS = {
     'DESCRIPTION': 'Happy Hours API',
     'VERSION': '1.0.0',
     'SERVE_INCLUDE_SCHEMA': False,
+    'SERVERS': [{'url': 'http://16.170.203.161/'}],
     'COMPONENTS': {
             'securitySchemes': {
                 'bearerAuth': {
