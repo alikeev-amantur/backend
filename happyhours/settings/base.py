0a512de6cbf0c616bcf4ccc6e76e8aa4bd06563a
unknown <amanturalikeev@gmail.com>
2024-04-29 12:30:54 +0600

    Fix swagger settings

--- happyhours/settings/base.py
+++ happyhours/settings/base.py
@@ -144,7 +144,7 @@ SPECTACULAR_SETTINGS = {
     'TITLE': 'Happy Hours',
     'DESCRIPTION': 'Happy Hours API',
     'VERSION': '1.0.0',
-    'SERVER': [
+    'SERVERS': [
             {'url': 'http://16.170.203.161', 'description': 'Production server'},
         ],
     'SERVE_INCLUDE_SCHEMA': False,
