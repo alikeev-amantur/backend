ca29cc6730f165b997b5b04daa1debc0ebab699b
Ataix <atai_ks@yahoo.com>
2024-05-15 19:59:08 +0600

    Feedback tests added

---
+++ apps/feedback/tests/test_views.py
@@ -0,0 +1,71 @@
+import pytest
+import pytz
+from django.db.models.functions import datetime
+from django.urls import reverse
+
+from freezegun import freeze_time
+from rest_framework import status
+from rest_framework.test import APIClient
+from happyhours.factories import (
+    UserFactory,
+    EstablishmentFactory,
+    FeedbackFactory,
+    FeedbackAnswerFactory,
+)
+
+
+@pytest.fixture
+def client():
+    return APIClient()
+
+
+@pytest.fixture
+def partner_user():
+    return UserFactory(role="partner")
+
+
+@pytest.fixture
+def normal_user():
+    return UserFactory(role="client")
+
+
+@pytest.fixture
+def partner_establishment(partner_user):
+    return EstablishmentFactory(owner=partner_user)
+
+
+@pytest.fixture
+def feedback(normal_user):
+    return FeedbackFactory(user=normal_user)
+
+
+@pytest.mark.django_db
+def test_retrieve_feedback(client, normal_user, feedback):
+    client.force_authenticate(user=normal_user)
+    url = reverse("v1:feedback-viewset", args=[feedback.id])
+    response = client.get(url)
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_create_feedback(client, normal_user, partner_establishment):
+    client.force_authenticate(user=normal_user)
+    url = reverse("v1:feedback-create")
+    data = {
+        "establishment": partner_establishment.id,
+        "text": "string"
+    }
+    response = client.post(url, data)
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_create_feedback_answer(client, normal_user, feedback):
+    client.force_authenticate(user=normal_user)
+    url = reverse("v1:feedback-answer-create")
+    data = {
+        "feedback": feedback.id,
+        "text": "string"
+    }
+    response = client.post(url, data)
+    assert response.status_code == status.HTTP_403_FORBIDDEN
