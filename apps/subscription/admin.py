ce86a22caa9b37098bfd4c4874619c496a921aee
unknown <amanturalikeev@gmail.com>
2024-06-07 16:28:55 +0600

    Update subscription serializer for ms format

--- apps/subscription/admin.py
+++ apps/subscription/admin.py
@@ -4,18 +4,4 @@ from apps.subscription.models import SubscriptionPlan, Subscription
 
 # Register your models here.
 admin.site.register(SubscriptionPlan)
-
-
-@admin.register(Subscription)
-class SubscriptionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'plan', 'start_date', 'end_date', 'is_active', 'is_trial')
-    search_fields = ('user__email', 'plan__name')
-
-    def formfield_for_dbfield(self, db_field, **kwargs):
-        formfield = super().formfield_for_dbfield(db_field, **kwargs)
-        if db_field.name in ['start_date', 'end_date']:
-            formfield.widget.attrs['step'] = '0.001'
-        return formfield
-
-
-
+admin.site.register(Subscription)
